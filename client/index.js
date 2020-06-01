bc275c90c9ad7ea6f916919029a3351d7258df4c
Serkan Sevilgen <ssevilgen@gmail.com>
2020-06-01 17:47:56 +0300

    accepts multiple config files

--- client/index.js
+++ client/index.js
@@ -1,10 +1,12 @@
 const osc = require('osc');
 
-const config = require('./config.json');
+process.argv[2];
+const config_file = process.argv[2] ? process.argv[2] : './config.json'; 
+const config = require(config_file);
 console.log(config);
 
 const oscPort = new osc.WebSocketPort({
-    url: `ws://${config.wsRemoteAddress}:${config.wsRemotePort}`,
+    url: `ws://${config.serverAddress}:${config.serverPort}`,
     metadata: true
 });
 
@@ -13,8 +15,8 @@ oscPort.open();
 // Start a UDP server listen to messages from audio app
 // and send it to the remote server
 const udpPort = new osc.UDPPort({
-    localAddress: config.udpServerAddress,
-    localPort: config.udpServerPort,
+    localAddress: config.clientAddress,
+    localPort: config.clientPort,
     metadata: true
 });
 
@@ -25,7 +27,7 @@ udpPort.open();
 // When there is message in UDP port, send an OSC message to remote server
 udpPort.on("ready", function () {
     udpPort.on("message", function (oscMsg, timeTag, info) {
-	console.log(oscMsg);
+        console.log("An OSC Message was received from APP!", oscMsg);
 	oscPort.send({
 	    address: oscMsg.address,
 	    args: oscMsg.args
@@ -35,7 +37,7 @@ udpPort.on("ready", function () {
 // When there is message frome remote server send it to audio app
 oscPort.on("ready", function () {
     oscPort.on("message", function (oscMsg) {
-        console.log("An OSC Message was received!", oscMsg);
+        console.log("An OSC Message was received from SERVER!", oscMsg);
 	udpPort.send({
 	    address: oscMsg.address,
 	    args: oscMsg.args
